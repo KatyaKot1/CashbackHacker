951b466a261ee020a0c82edfc762ba1e931d800f
Екатерина Бакланова <ekakotelevskaya@yandex.ru>
2022-09-09 18:05:08 +0300

    new tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,10 +1,12 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
+
     @Test
     public void shouldNotOfferToBuyIfBoundary() {
         CashbackHackService service = new CashbackHackService();
@@ -28,10 +30,29 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void ShouldOfferToBuyUpToTwoThousands() {
+    public void shouldOfferToBuyUpToTwoThousands() {
         CashbackHackService service = new CashbackHackService();
         int expected = 900;
         int amount = 1100;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldOfferToBuyGoodsOnOneRuble() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 1;
+        int amount = 999;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+
+    }
+    @Test
+    public void shouldOfferToBuyWithZero() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 1000;
+        int amount = 0;
         int actual = service.remain(amount);
 
         assertEquals(actual, expected);
